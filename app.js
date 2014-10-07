5fc74a5936d714223d2a9f0ef6a185d945003b82
Vitor Leonel <vrleonel@gmail.com>
2014-10-07 18:12:42 -0300

    Criando novas caracteristicas para  a loja

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-var app = angular.module('store', []);
+(function () {
+    var app = angular.module('store', []);
+
+
+    app.controller('StoreController', function(){
+            this.products = gems;
+    });
+
+    var gems = [
+    {
+        name: 'Dedecahedron',
+        price: 6,
+        description: 'lorem ipsum',
+        soldOut: true,
+        images: [
+            {
+                full:  'static/img/city-full-1.jpeg',
+                thumb: 'static/img/city-thumb-1.jpeg',
+            }
+        ],
+    },
+    {
+        name: 'Pentagon Gem',
+        price: 5.95,
+        description: 'lorem ipsum',
+        soldOut: false,
+        images: [
+            {
+                full:  'static/img/city-full-2.jpeg',
+                thumb: 'static/img/city-thumb-2.jpeg',
+            }
+        ],
+    }
+    ];
+
+})();
